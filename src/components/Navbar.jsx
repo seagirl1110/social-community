ba5e80d41aef254883e91945568f90ff2a173935
Marina <seagirl1110@gmail.com>
2023-05-02 12:21:51 +0300

    feat: start project 'social community'

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import './Navbar.scss';
+
+const Navbar = () => {
+  const items = ['Profile', 'Messages', 'News', 'Music', 'Settings'];
+  return (
+    <aside className="navbar">
+      <ul className="navbar__list nav-list">
+        {items.map((item) => {
+          return (
+            <li className="nav-list__item">
+              <a className="nav-list__link" href="#s">
+                {item}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
+
+export default Navbar;
