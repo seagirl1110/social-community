ad4d1d5ded4c99c74ccc81c7681f319e30c809bb
Marina <seagirl1110@gmail.com>
2023-07-14 11:54:25 +0300

    refactor: delete rerenderTree

--- src/index.js
+++ src/index.js
@@ -7,18 +7,13 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-export const rerenderTree = () => {
-  root.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>
-  );
-};
-
-rerenderTree();
-store.subscribe(rerenderTree);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
